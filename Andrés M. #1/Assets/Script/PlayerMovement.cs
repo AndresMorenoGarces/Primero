36d442f7f7d9ec9195ff381a74289684f99e47d1
Luxfero000 <Luxfero000@users.noreply.github.com>
2018-09-06 18:14:34 -0500

    1.0.0
    Base

---
+++ Andrés M. #1/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float velocidad;
+    public float rotateSpeed = 3.0F;
+    CharacterController controller;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+    void Update()
+    {
+            Movimiento();
+    }
+
+    public void Movimiento()
+    { //Movimiento del personaje
+            transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+            Vector3 forward = transform.TransformDirection(Vector3.forward);
+            float curSpeed = velocidad * Input.GetAxis("Vertical");
+            controller.SimpleMove(forward * curSpeed);
+
+    }
+    
+}
+    
+
+
