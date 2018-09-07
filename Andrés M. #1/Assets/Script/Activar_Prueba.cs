e9a163f369fe9c6116d8a38214a58e1f455bdd36
Luxfero000 <Luxfero000@users.noreply.github.com>
2018-09-06 20:52:41 -0500

    1.0.1

---
+++ Andrés M. #1/Assets/Script/Activar_Prueba.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activar_Prueba : MonoBehaviour
+{
+    public GameObject prueba;
+
+	private void Start ()
+    {
+        prueba.SetActive(false);
+	}
+
+    private void OnTriggerEnter(Collider sensor)
+    {
+        if (sensor.transform.tag == "Player")
+        {
+            prueba.SetActive(true);
+        }
+    }
+    private void OnTriggerExit(Collider sensor)
+    {
+        if (sensor.transform.tag == "Player")
+        {
+            prueba.SetActive(false);
+        }
+    }
+     
+}
+
