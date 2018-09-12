12ec0cbf59024ebe6cf81decb725c0e80797de1d
Luxfero000 <Luxfero000@users.noreply.github.com>
2018-09-12 15:41:17 -0500

    1.0.5

---
+++ Andrés M. #1/Assets/Script/Desplazamiento_Horizontal.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Desplazamiento_Horizontal : MonoBehaviour
+{
+	public bool canX;
+	public float speed_X;
+	public float amount_x;
+	float m_internal_amount_x;
+	public bool right;
+
+	private void Start()
+	{
+		m_internal_amount_x = amount_x;
+	}
+
+	private void Update()
+	{
+		if (canX)
+		{
+			MoveX();
+		}
+	}
+
+	void MoveX()
+	{
+		if (right)
+		{
+			transform.Translate(Vector3.right * (speed_X * Time.deltaTime), Space.Self);
+			amount_x -= Time.deltaTime;
+			if (amount_x < m_internal_amount_x*-1)
+			{
+				right = false;
+			}
+
+		}
+		else
+		{
+			transform.Translate(Vector3.left * (speed_X * Time.deltaTime), Space.Self);
+			amount_x += Time.deltaTime;
+			if (amount_x > m_internal_amount_x)
+			{
+				right = true;
+			}
+		}
+	}
+}
---
+++ Andrés M. #1/Assets/Script/Desplazamiento_Vertical.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Desplazamiento_Vertical : MonoBehaviour
+{
+	public bool canY;
+	public float speed_y;
+	public float amount_y;
+	float m_internal_amount_y;
+	public bool up;
+
+	private void Start()
+	{
+		m_internal_amount_y = amount_y;
+	}
+
+	private void Update()
+	{
+		if (canY)
+		{
+			MoveY();
+		}
+	}
+
+	void MoveY()
+	{
+		if (up)
+		{
+			transform.Translate(Vector2.up * (speed_y * Time.deltaTime), Space.Self);
+			amount_y -= Time.deltaTime;
+			if (amount_y < m_internal_amount_y * -1)
+			{
+				up = false;
+			}
+
+		}
+		else
+		{
+			transform.Translate(Vector2.down * (speed_y * Time.deltaTime), Space.Self);
+			amount_y += Time.deltaTime;
+			if (amount_y > m_internal_amount_y)
+			{
+				up = true;
+			}
+		}
+	}
+}
